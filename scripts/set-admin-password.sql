0463e1a5f63452a9093e0f96bdcf390b91967bfb
murat-kacar <murat.kacar59@gmail.com>
2026-02-06 21:28:11 +0000

    Cleaned up unnecessary files and prepared project

--- scripts/set-admin-password.sql
+++
@@ -1,5 +0,0 @@
--- Run this against a Postgres database to set the admin user's password hash
-
-UPDATE users
-SET password_hash = '$2b$10$SsU3rxUD3GAkirXXQplyLuBLsEdrXouUatMuWLAWQlRq7pplXMOM6'
-WHERE username = 'admin';
--- tmp/genhash.js
+++
@@ -1,6 +0,0 @@
-const bcrypt = require('bcryptjs');
-const pw = process.argv[2] || 'Admin123!';
-bcrypt.hash(pw, 10, (err, hash) => {
-  if (err) { console.error(err); process.exit(1); }
-  console.log(hash);
-});
